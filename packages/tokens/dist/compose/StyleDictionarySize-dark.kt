562ba8f8fbf36577b9e8601140be609fa7a7e87a
hieud4l <74419980+hieud4l@users.noreply.github.com>
2026-04-02 11:25:02 +0700

    chore: add tokens dist build output

---
+++ packages/tokens/dist/compose/StyleDictionarySize-dark.kt
@@ -0,0 +1,112 @@
+
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+package com.yourapp.tokens
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionarySize {
+  val dimensionFontSizeDisplay2xl = 1152.00.dp
+  val dimensionFontSizeDisplayLg = 768.00.dp
+  val dimensionFontSizeDisplayMd = 576.00.dp
+  val dimensionFontSizeDisplaySm = 480.00.dp
+  val dimensionFontSizeDisplayXl = 960.00.dp
+  val dimensionFontSizeDisplayXs = 384.00.dp
+  val dimensionFontSizeTextLg = 288.00.dp
+  val dimensionFontSizeTextMd = 256.00.dp
+  val dimensionFontSizeTextSm = 224.00.dp
+  val dimensionFontSizeTextXl = 320.00.dp
+  val dimensionFontSizeTextXs = 192.00.dp
+  val dimensionLayoutContainerMaxWidthDesktop = 20480.00.dp
+  val dimensionLayoutContainerPaddingDesktop = 512.00.dp
+  val dimensionLayoutContainerPaddingMobile = 256.00.dp
+  val dimensionLayoutParagraphMaxWidth = 11520.00.dp
+  val dimensionLineHeightDisplay2xl = 1440.00.dp
+  val dimensionLineHeightDisplayLg = 960.00.dp
+  val dimensionLineHeightDisplayMd = 704.00.dp
+  val dimensionLineHeightDisplaySm = 608.00.dp
+  val dimensionLineHeightDisplayXl = 1152.00.dp
+  val dimensionLineHeightDisplayXs = 512.00.dp
+  val dimensionLineHeightTextLg = 448.00.dp
+  val dimensionLineHeightTextMd = 384.00.dp
+  val dimensionLineHeightTextSm = 320.00.dp
+  val dimensionLineHeightTextXl = 480.00.dp
+  val dimensionLineHeightTextXs = 288.00.dp
+  val dimensionRadius2xl = 256.00.dp
+  val dimensionRadius3xl = 320.00.dp
+  val dimensionRadius4xl = 384.00.dp
+  val dimensionRadiusFull = 159984.00.dp
+  val dimensionRadiusLg = 160.00.dp
+  val dimensionRadiusMd = 128.00.dp
+  val dimensionRadiusNone = 0.00.dp
+  val dimensionRadiusSm = 96.00.dp
+  val dimensionRadiusXl = 192.00.dp
+  val dimensionRadiusXs = 64.00.dp
+  val dimensionRadiusXxs = 32.00.dp
+  val dimensionSpacing00px = 0.00.dp
+  val dimensionSpacing052px = 32.00.dp
+  val dimensionSpacing1040px = 640.00.dp
+  val dimensionSpacing120480px = 7680.00.dp
+  val dimensionSpacing1248px = 768.00.dp
+  val dimensionSpacing140560px = 8960.00.dp
+  val dimensionSpacing14px = 64.00.dp
+  val dimensionSpacing156px = 96.00.dp
+  val dimensionSpacing160640px = 10240.00.dp
+  val dimensionSpacing1664px = 1024.00.dp
+  val dimensionSpacing180720px = 11520.00.dp
+  val dimensionSpacing192768px = 12288.00.dp
+  val dimensionSpacing2080px = 1280.00.dp
+  val dimensionSpacing2496px = 1536.00.dp
+  val dimensionSpacing2510px = 160.00.dp
+  val dimensionSpacing2561024px = 16384.00.dp
+  val dimensionSpacing28px = 128.00.dp
+  val dimensionSpacing312px = 192.00.dp
+  val dimensionSpacing3201280px = 20480.00.dp
+  val dimensionSpacing32128px = 2048.00.dp
+  val dimensionSpacing3601440px = 23040.00.dp
+  val dimensionSpacing4001600px = 25600.00.dp
+  val dimensionSpacing40160px = 2560.00.dp
+  val dimensionSpacing416px = 256.00.dp
+  val dimensionSpacing4801920px = 30720.00.dp
+  val dimensionSpacing48192px = 3072.00.dp
+  val dimensionSpacing520px = 320.00.dp
+  val dimensionSpacing56224px = 3584.00.dp
+  val dimensionSpacing624px = 384.00.dp
+  val dimensionSpacing64256px = 4096.00.dp
+  val dimensionSpacing80320px = 5120.00.dp
+  val dimensionSpacing832px = 512.00.dp
+  val dimensionSpacing96384px = 6144.00.dp
+  val dimensionSpacingSemantic10xl = 2048.00.dp
+  val dimensionSpacingSemantic11xl = 2560.00.dp
+  val dimensionSpacingSemantic2xl = 320.00.dp
+  val dimensionSpacingSemantic3xl = 384.00.dp
+  val dimensionSpacingSemantic4xl = 512.00.dp
+  val dimensionSpacingSemantic5xl = 640.00.dp
+  val dimensionSpacingSemantic6xl = 768.00.dp
+  val dimensionSpacingSemantic7xl = 1024.00.dp
+  val dimensionSpacingSemantic8xl = 1280.00.dp
+  val dimensionSpacingSemantic9xl = 1536.00.dp
+  val dimensionSpacingSemanticLg = 192.00.dp
+  val dimensionSpacingSemanticMd = 128.00.dp
+  val dimensionSpacingSemanticMd2 = 160.00.dp
+  val dimensionSpacingSemanticNone = 0.00.dp
+  val dimensionSpacingSemanticSm = 96.00.dp
+  val dimensionSpacingSemanticXl = 256.00.dp
+  val dimensionSpacingSemanticXs = 64.00.dp
+  val dimensionSpacingSemanticXxs = 32.00.dp
+  val dimensionWidth2xl = 16384.00.dp
+  val dimensionWidth3xl = 20480.00.dp
+  val dimensionWidth4xl = 23040.00.dp
+  val dimensionWidth5xl = 25600.00.dp
+  val dimensionWidth6xl = 30720.00.dp
+  val dimensionWidthLg = 10240.00.dp
+  val dimensionWidthMd = 8960.00.dp
+  val dimensionWidthSm = 7680.00.dp
+  val dimensionWidthXl = 12288.00.dp
+  val dimensionWidthXs = 6144.00.dp
+  val dimensionWidthXxs = 5120.00.dp
+}
